58159afd6b4fddd75e23443678e0b4ca407ce549
seahawksRawesome <league@iMac-7.attlocal.net>
2019-02-10 17:05:27 -0800

    finished this

--- test/HashMapCalculator.java
+++ test/HashMapCalculator.java
@@ -4,13 +4,17 @@ public class HashMapCalculator {
 //Compare the key of i of hashmap 1 with the key at i with hashmap 2. If same add one to total. At end reutrn it.
 	int total = 0;
 	public int commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
-		String[] hash1 = (String[]) hashmap1.keySet().toArray();
-		String[] hash2 = (String[]) hashmap2.keySet().toArray();
-		for(int i  = 0; i < hashmap1.size(); i++) {
-			
-			for(int j = 0; j < hashmap1.size(); j++) {
-				if(hashmap1.get(i) == hashmap2.get(j)) {
-					
+		Object[] hash1 = (Object[]) hashmap1.keySet().toArray();
+		Object[] hash2 = (Object[]) hashmap2.keySet().toArray();
+		int set = 0;
+		for(int i  = 0; i < hash1.length-1; i++) {
+			set = i;
+			for(int j = 0; j < hash1.length-1; j++) {
+				if(hashmap1.get(set) == hashmap2.get(j) && hash1[set] == hash2[j]) {
+					//if(hashmap1.get(set))
+					//total++;
+						total+=1;
+		
 				}
 				else {
 					System.out.println("Nope");
